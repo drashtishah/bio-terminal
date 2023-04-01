86976a87a4fc924a1e3366c90499cb071c170b99
Drashti Shah <drashti@inkredo.in>
2023-04-01 15:53:53 +0530

    Initial setup

---
+++ bio-terminal/target/debug/deps/bio_terminal-982615ccc5707129.d
@@ -0,0 +1,5 @@
+/Users/drashti/Developer/bio-terminal/bio-terminal/target/debug/deps/bio_terminal-982615ccc5707129.rmeta: src/main.rs
+
+/Users/drashti/Developer/bio-terminal/bio-terminal/target/debug/deps/bio_terminal-982615ccc5707129.d: src/main.rs
+
+src/main.rs:
---
+++ bio-terminal/target/debug/deps/bio_terminal-fd6d67416f08c53d.d
@@ -0,0 +1,5 @@
+/Users/drashti/Developer/bio-terminal/bio-terminal/target/debug/deps/bio_terminal-fd6d67416f08c53d.rmeta: src/main.rs
+
+/Users/drashti/Developer/bio-terminal/bio-terminal/target/debug/deps/bio_terminal-fd6d67416f08c53d.d: src/main.rs
+
+src/main.rs:
