e916348bf9b2deacfbc192f7c8af4e57073d07fb
Drashti Shah <drashti@inkredo.in>
2023-04-02 08:45:33 +0530

    Fix the sequence of prompts on terminal

--- bio-terminal/src/main.rs
+++ bio-terminal/src/main.rs
@@ -5,11 +5,14 @@ use utils::{print_challenges, function_map};
 fn main() {
     let mut x = 0;
     let solutions = function_map();
+    let mut input_list = Vec::new();
     let welcome_message: &str = "This is your bio-terminal.\nYou can analyse biological data here.\nWould you like to start by solving challenges on Rosalind?\nPlease enter Y to start.\nOr enter exit to close the bio-terminal.";
 
     loop {
         if x == 0 {
+            // Print the welcome message only once
             println!("{}", welcome_message);
+            x = x + 1;
         }
 
         let mut input = String::new();
@@ -17,21 +20,27 @@ fn main() {
             .read_line(&mut input)
             .expect("There was an error while reading your input data.");
         input = input.trim().to_string();
+        let num: i32 = input.parse().unwrap_or_default();
 
         if input == "exit" {
             break;
-        } else if input == "Y" || input == "Yes" || input == "y" {
-            x = x + 1;
+        } else if input == "Y" {
+            println!("{}", "Select the problem you want to solve.");
             print_challenges();
             print!("Enter a number: ");
             io::stdout().flush().expect("Error flushing stdout");
-        } else if input == "N" || input == "No" || input == "n" {
+            let temp_input = input.clone();
+            input_list.push(temp_input);
+        } else if input == "N" {
             println!("{}", "Bio-Terminal is a work-in-progress. You can only solve Rosalind challenges right now.");
             break;
+        } else if num == 0 {
+            // Any other text input
+            println!("Please check your input.");
         }
 
-        let num: i32 = input.parse().unwrap_or_default();
-        if num > 0 {
+        let yes: String = "Y".to_string();
+        if num > 0 && input_list.contains(&yes) {
             if let Some(func) = solutions.get(&num) {
                 print!("Enter the input sequence: ");
                 io::stdout().flush().expect("Error flushing stdout");
@@ -40,11 +49,17 @@ fn main() {
                 .read_line(&mut input)
                 .expect("There was an error while reading your input data.");
                 input = input.trim().to_string();
+                if input == "exit" {break;}
                 let result = func(&input);
-                println!("{:?}", result);
+                println!("Your solution for question {} -> {:?}", num, result);
+                println!("Please select the next challenge you want to solve.");
+                print_challenges();
+            }  else {
+                println!("This question does not seem to be in the list.");
+            }
+        } else if num > 0 {
+            // For numeric inputs typed in before a "Y"
+            println!("Please check your input.");
         }
-
-        x = x + 1;
-    }
 }
 }
--- bio-terminal/src/utils.rs
+++ bio-terminal/src/utils.rs
@@ -2,17 +2,12 @@ use std::collections::HashMap;
 use rosalind::count_nucleotides;
 
 pub fn print_challenges() {
-    println!("{}", "Select the problem you want to solve.");
     println!("{}", "1. Counting DNA Nucleotides");
+    println!("{}", "2. Transcribing DNA into RNA");
 }
 
 pub fn function_map() -> HashMap<i32, fn(&str) -> HashMap<char, usize>> {
     let mut function_map: HashMap<i32, fn(&str) -> HashMap<char, usize>> = HashMap::new();
     function_map.insert(1, count_nucleotides);
     function_map
-}
-
-pub fn run_solution(num: i32, sequence: &str) {
-    
-    
 }
